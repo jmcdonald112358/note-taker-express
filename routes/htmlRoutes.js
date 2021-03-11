412c253595f0d835409289f54e6f2ea57893338f
Justin McDonald <73570812+jmcdonald112358@users.noreply.github.com>
2021-03-11 00:59:33 -0700

    Clarified comments

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,12 @@ const path = require('path');
 
 module.exports = (app) => {
 
-   //GET /notes returns ../public/notes.html
+   //Route to load the notes.html file
    app.get('/notes', (req, res) => {
       res.sendFile(path.join(__dirname, '../public/notes.html'));
    });  
    
-   //GET * returns ../public/index.html
+   //Route to load the index.html file
    app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../public/index.html'));
    });
