8409e199426bda768fc13d8572701be68219a787
Justin McDonald <73570812+jmcdonald112358@users.noreply.github.com>
2021-03-11 01:06:43 -0700

    Removing unnecessary comments and clarifying other comments

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 // Call dependencies
 const express = require('express');
 const app = express();
-// const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes');
 const PORT = process.env.PORT || 8080;
 
-// Set up app to handle data parsing
+// Define middleware for data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Map to route files
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
-
+// Map to routing files
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
